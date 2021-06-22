c2f9253f8681b3b9088128140825179f758b4232
iikim <iikim2511@gmail.com>
2021-06-22 14:15:55 -0400

    //🍀button-detail

--- script.js
+++ script.js
@@ -25,15 +25,18 @@ const submitBtn = document.querySelector('.submit');
 const clearAllBtn = document.querySelector('.clear_all');
 
 const noteNew = document.querySelector('#note_new');
+const noteDetailNew = document.querySelector('#note_detail_new');
+
 
 //🍀
 
 /* 🍄Algorithm) 
 2. submit- title, note - create
-3. clear all
+3. save local storage : so that I can use the data for detail div
 4. click btn in created note  - detail, delete
 6. detail btn - show whole note
 8. create delete btn in detail note and give function
+10 clear all button
 */
 
 //🍀 2. title, note - create
@@ -44,16 +47,39 @@ function run(e){
        
     let note = document.createElement('div');
     note.classList.add('note_new_container');
+
+    //🦄.substring();
     note.innerHTML=`    
-        <h2>${titleInput.value}</h2>
-        <div class="content">${noteInput.value}</div>
+        <h2>${titleInput.value.substring(0,20)}</h2> 
+        <div class="content">${noteInput.value.substring(0,50)}...</div>
         <button class="myButton2 detail">view detail</button>
         <button class="myButton2 delete">delete</button>    
     `
     noteNew.append(note);   
+    
+
+    //🍀3. save local storage : so that I can use the data for detail div
+
+    //🍀button-detail
+    let detailBtn =  document.querySelector('.detail');
+    detailBtn.addEventListener('click',(e)=>{
+
+        e.preventDefault();
+
+        let note_detail = document.createElement('div');
+        note_detail.classList.add('note_detail_container');
+
+        note_detail.innerHTML=`
+            <h2>${titleInput.value}</h2> 
+            <div class="content">${noteInput.value}...</div>
+        `;
+       noteDetailNew.append(note_detail);
+
+    });
 
     titleInput.value="" ;
     noteInput.value="" ;
+    
 }
 
 //🍀3 reload
